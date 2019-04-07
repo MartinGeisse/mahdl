99dfd5e2620c7d14c890118e4d6377fd7286b9b3
Martin Geisse <martingeisse@googlemail.com>
2019-04-07 13:42:34 +0200

    intellij compatibility in gradle plugin, WIP

--- mahdl-gradle/src/main/groovy/name/martingeisse/mahdl/gradle/MahdlGradlePlugin.groovy
+++ mahdl-gradle/src/main/groovy/name/martingeisse/mahdl/gradle/MahdlGradlePlugin.groovy
@@ -1,6 +1,5 @@
 package name.martingeisse.mahdl.gradle
 
-import name.martingeisse.mahdl.plugin.CodeGenerator
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -9,21 +8,15 @@ import org.gradle.api.Project
  */
 class MahdlGradlePlugin implements Plugin<Project> {
 
-	MahdlGradlePlugin() {
-	}
-
 	void apply(Project project) {
-
 		project.task('mahdlCodegen') {
 			group 'build'
 			description 'Generates Java code from MaHDL sources.'
 			project.tasks.compileJava.dependsOn(delegate)
 			doLast {
-				new CodeGenerator().run(project.projectDir)
-				CompilationErrors.failOnErrors();
+				MahdlGradleMain.main(project.projectDir);
 			}
 		}
-
 	}
 
 }
---
+++ mahdl-gradle/src/main/java/name/martingeisse/mahdl/gradle/MahdlGradleMain.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018 Martin Geisse
+ * This file is distributed under the terms of the MIT license.
+ */
+package name.martingeisse.mahdl.gradle;
+
+import java.io.File;
+
+/**
+ *
+ */
+public class MahdlGradleMain {
+
+	public static void main(File projectDir) {
+		// TODO ...
+		CompilationErrors.failOnErrors();
+	}
+
+}
