f540802b6fb4e2ca38e16032b456bfde6c98f477
Martin Geisse <martingeisse@googlemail.com>
2019-04-07 11:26:24 +0200

    intellij compatibility in gradle plugin, WIP

--- mahdl-gradle/src/main/java/name/martingeisse/mahdl/intellij/PsiBuilderImpl.java
+++ mahdl-gradle/src/main/java/name/martingeisse/mahdl/intellij/PsiBuilderImpl.java
@@ -21,9 +21,11 @@ public class PsiBuilderImpl implements PsiBuilder {
 	private final ImmutableList<Token> tokens;
 	private int tokenIndex = 0;
 	private final List<Marker> markerStack = new ArrayList<>();
+	private final ASTNode rootNode;
 
-	public PsiBuilderImpl(ImmutableList<Token> tokens) {
+	public PsiBuilderImpl(ImmutableList<Token> tokens, IElementType rootElementType) {
 		this.tokens = tokens;
+		this.rootNode = new ASTNode(rootElementType, 0, 0);
 	}
 
 	@Override
@@ -35,12 +37,17 @@ public class PsiBuilderImpl implements PsiBuilder {
 
 	@Override
 	public IElementType getTokenType() {
-		return (tokenIndex < tokens.size() ? tokens.get(tokenIndex).getElementType() : null;
+		return (tokenIndex < tokens.size() ? tokens.get(tokenIndex).getElementType() : null);
 	}
 
 	@Override
 	public Marker mark() {
-		TODO
+		if (tokenIndex == tokens.size()) {
+			throw new IllegalStateException("cannot mark at EOF");
+		}
+		MarkerImpl marker = new MarkerImpl(tokens.get(tokenIndex));
+		markerStack.add(marker);
+		return marker;
 	}
 
 	@Override
@@ -55,19 +62,55 @@ public class PsiBuilderImpl implements PsiBuilder {
 
 	@Override
 	public ASTNode getTreeBuilt() {
-		TODO
+		return rootNode;
 	}
 
 	public final class MarkerImpl implements Marker {
 
+		private final Token startToken;
+		private final List<ASTNode> unlinkedChildNodes = new ArrayList<>();
+
+		public MarkerImpl(Token startToken) {
+			this.startToken = startToken;
+		}
+
 		@Override
 		public void rollbackTo() {
-			TODO
+			int index = markerStack.lastIndexOf(this);
+			if (index == -1) {
+				throw new IllegalStateException("this marker is not active");
+			}
+			while (markerStack.size() >= index) {
+				markerStack.remove(markerStack.size() - 1);
+			}
 		}
 
 		@Override
 		public void done(IElementType type) {
-			TODO
+			if (getTopOfStack() != this) {
+				throw new IllegalStateException("nested markers are active");
+			}
+			markerStack.remove(markerStack.size() - 1);
+			if (markerStack.isEmpty()) {
+				rootNode.createChild(startToken);
+			} else {
+				MarkerImpl parentMarker = (MarkerImpl)markerStack.get(markerStack.size() - 1);
+				ASTNode node = parentMarker.createUnlinkedChildNode(startToken);
+				for (ASTNode unlinkedChildNode : unlinkedChildNodes) {
+					node.createChild(unlinkedChildNode.getElementType(), unlinkedChildNode.getRow(), unlinkedChildNode.getColumn());
+				}
+			}
+		}
+
+		private Marker getTopOfStack() {
+			int size = markerStack.size();
+			return (size == 0 ? null : markerStack.get(size - 1));
+		}
+
+		private ASTNode createUnlinkedChildNode(Token childToken) {
+			ASTNode node = new ASTNode(childToken);
+			unlinkedChildNodes.add(node);
+			return node;
 		}
 
 	}
