1e28eee5153f11f0489ee9305046419d01475164
Martin Geisse <martin.geisse@servicereisen.de>
2019-04-16 12:35:58 +0200

    gradle plugin, WIP

--- mahdl-gradle/src/main/java/name/martingeisse/mahdl/gradle/ProcessingRun.java
+++ mahdl-gradle/src/main/java/name/martingeisse/mahdl/gradle/ProcessingRun.java
@@ -3,6 +3,7 @@ package name.martingeisse.mahdl.gradle;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import name.martingeisse.mahdl.input.cm.Module;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.util.HashMap;
@@ -28,11 +29,36 @@ public class ProcessingRun {
 
 	public void run() {
 		generatedCode = new HashMap<>();
+		for (Map.Entry<ImmutableList<String>, Module> codeModelEntry : codeModels.entrySet()) {
+			ImmutableList<String> qualifiedName = codeModelEntry.getKey();
+			Module moduleCm = codeModelEntry.getValue();
 
-		// TODO test
-		ImmutableList<String> key = ImmutableList.of("test", "Dummy");
-		String code = "package test; public class Dummy {public static void foo() {System.out.println(\"FOO\");}}";
-		generatedCode.put(key, code);
+			ImmutableList<String> packageName = qualifiedName.subList(0, qualifiedName.size() - 1);
+			String localName = qualifiedName.get(qualifiedName.size() - 1);
+
+			StringBuilder builder = new StringBuilder();
+			if (!packageName.isEmpty()) {
+				builder.append("package ").append(StringUtils.join(packageName, '.')).append(";\n");
+				builder.append("\n");
+			}
+			builder.append("import name.martingeisse.esdk.core.rtl.*;\n");
+			builder.append("import name.martingeisse.esdk.core.rtl.synthesis.verilog.*;\n");
+			builder.append("\n");
+			builder.append("public class ").append(localName).append(" extends RtlItem {\n");
+			builder.append("\n");
+			builder.append("	public ").append(localName).append("(RtlRealm realm) {\n");
+			builder.append("		super(realm);\n");
+			builder.append("	}\n");
+			builder.append("\n");
+			builder.append("	@Override\n");
+			builder.append("	public VerilogContribution getVerilogContribution() {\n");
+			builder.append("		return new EmptyVerilogContribution();\n");
+			builder.append("	}\n");
+			builder.append("\n");
+			builder.append("}\n");
+
+			generatedCode.put(qualifiedName, builder.toString());
+		}
 
 	}
 
